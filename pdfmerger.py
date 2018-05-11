2994c1987fcfb1fde052e26e54b937df86406b9c
ngurnani <ngurnani@gmail.com>
2018-05-10 23:59:58 -0700

    wrote basic script to merge pdf files in a folder, need to clean up and make more useful

---
+++ pdfmerger.py
@@ -0,0 +1,13 @@
+# simple script to merge pdf files in directory
+
+import os
+from os import listdir
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
+target_directory = '/Users/ngurnani/Desktop/cambridge-core_bootstrap-methods-and-their-application_11May2018'
+filenames = [f for f in listdir(target_directory)]
+merger = PdfFileMerger()
+for filename in filenames:
+    merger.append(PdfFileReader(file(filename, 'rb')))
+
+merger.write("document-output.pdf")
