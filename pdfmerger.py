8bc3e587dbbce26a46970b7ff101221d33af424b
ngurnani <ngurnani@gmail.com>
2018-05-11 00:57:31 -0700

    added a ipython notebook version of pdfmerger, much easier to debug in an interactive environment

--- pdfmerger.py
+++ pdfmerger.py
@@ -4,10 +4,11 @@ import os
 from os import listdir
 from PyPDF2 import PdfFileMerger, PdfFileReader
 
-target_directory = '/Users/ngurnani/Desktop/cambridge-core_bootstrap-methods-and-their-application_11May2018'
+target_directory = '/Users/ngurnani/Desktop/AssetPriceDynamics'
 os.chdir(target_directory)
 
 filenames = [f for f in listdir(target_directory)]
+filenames.sort() # pdfs sorted in correct order
 merger = PdfFileMerger()
 for filename in filenames:
     merger.append(PdfFileReader(open(filename, 'rb')))
