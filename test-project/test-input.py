2ef73912ef0338fb29e2b2f82d762e6ba7ae8349
ngurnani <ngurnani@gmail.com>
2018-08-16 22:06:51 -0400

    added test project to experiment in

---
+++ test-project/test-input.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+    target_directory = sys.argv[1]
+    print(target_directory)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test-project/test-module.py
@@ -0,0 +1,32 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+
+class Employee:
+    """A sample Employee class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def test_method(self):
+        pass
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+for num in [1, 2, 3]:
+    print(num)
+
+emp_1 = Employee('John', 'Smith')
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
