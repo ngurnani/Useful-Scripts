38e75f6d867830f4bb7118aaadb9b3d7c9b303bc
ngurnani <ngurnani@gmail.com>
2020-08-17 17:19:09 -0400

    add fileprefix filter, make backwards compatible with python2.7

--- get_files.py
+++ get_files.py
@@ -9,16 +9,17 @@ from decorators import timer
 
 
 @timer
-def get_files(filepath, filetype="json"):
+def get_files(filepath, fileprefix="", filetype="json"):
     """
     return all files of  filetype in filepath
     :param filepath: str
+    :param fileprefix: str
     :param filetype: str (json, pdf, xlsx)
     :return: list of files
     """
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, f"*.{filetype}"))
+        files = glob.glob(os.path.join(root, "{0}*.{1}".format(fileprefix, filetype)))
         for f in files:
             all_files.append(os.path.abspath(f))
     return all_files
